62e51c8c80ed5255b9d2102dd346fee2c89cfafa
William Infante <williaminfante@gmail.com>
2021-05-02 15:21:27 +1000

    feat: move maintenance to subfolder

---
+++ maintenance/go.mod
@@ -0,0 +1,8 @@
+module github.com/williaminfante/go_test_mocking/maintenance
+
+go 1.16
+
+require (
+	github.com/stretchr/testify v1.7.0
+	github.com/williaminfante/go_test_mocking v0.0.0-20210502051441-176bd99eba47
+)
--- maintenance/maintenance_test.go
+++ maintenance/maintenance_test.go
@@ -5,31 +5,31 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	example "github.com/williaminfante/go_test_mocking"
+	"github.com/williaminfante/go_test_mocking/maintenance"
 )
 
 func TestNeedsMaintenace(t *testing.T) {
 	t.Run("when days is just 30", func(t *testing.T) {
-		needsMaintenance, actualError := example.NeedsMaintenace(30)
+		needsMaintenance, actualError := maintenance.NeedsMaintenace(30)
 		assert.Equal(t, true, needsMaintenance)
 		assert.Equal(t, nil, actualError)
 	})
 	t.Run("when days is above 30", func(t *testing.T) {
-		needsMaintenance, actualError := example.NeedsMaintenace(42)
+		needsMaintenance, actualError := maintenance.NeedsMaintenace(42)
 		assert.Equal(t, true, needsMaintenance)
 		assert.Equal(t, nil, actualError)
 	})
 	t.Run("when days is less than 30", func(t *testing.T) {
-		needsMaintenance, actualError := example.NeedsMaintenace(24)
+		needsMaintenance, actualError := maintenance.NeedsMaintenace(24)
 		assert.Equal(t, false, needsMaintenance)
 		assert.Equal(t, nil, actualError)
-		zeroMaintenance, zeroError := example.NeedsMaintenace(0)
+		zeroMaintenance, zeroError := maintenance.NeedsMaintenace(0)
 		assert.Equal(t, false, zeroMaintenance)
 		assert.Equal(t, nil, zeroError)
 	})
 	t.Run("when days is less than 0", func(t *testing.T) {
 		expectedError := errors.New("cannot accept less than zero days")
-		needsMaintenance, actualError := example.NeedsMaintenace(-42)
+		needsMaintenance, actualError := maintenance.NeedsMaintenace(-42)
 		assert.Equal(t, false, needsMaintenance)
 		assert.Equal(t, expectedError, actualError)
 	})
