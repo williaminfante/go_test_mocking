029ff1822e7dbd3d8c66e6d9ee0c1b4b2fa66568
William Infante <williaminfante@gmail.com>
2021-05-03 15:51:08 +1000

    feat: changed to no mocks making it tightly coupled

--- src/vehicle/vehicle_test.go
+++ src/vehicle/vehicle_test.go
@@ -5,38 +5,29 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/williaminfante/go_test_mocking/mocks"
 	"github.com/williaminfante/go_test_mocking/src/vehicle"
 )
 
 func TestConductChecks(t *testing.T) {
-	testM := new(mocks.MockedMaintenanceWrapper)
-	maintenanceRepository := vehicle.MaintenanceWrapper{Maintenance: testM}
 	t.Run("no mocking testing", func(t *testing.T) {
 		t.Run("maintenance", func(t *testing.T) {
-			testM.On("NeedsMaintenance", 45).Return(true).Once()
-			prescription, err := maintenanceRepository.ConductChecks("lightning", 45)
-			testM.AssertExpectations(t)
+			prescription, err := vehicle.ConductChecks("lightning", 45)
 			assert.Equal(t,
 				"lightning is up for maintenance",
 				prescription)
 			assert.Equal(t, nil, err)
 		})
 		t.Run("good", func(t *testing.T) {
-			testM.On("NeedsMaintenance", 23).Return(false).Once()
-			prescription, err := maintenanceRepository.ConductChecks("lightning", 23)
-			testM.AssertExpectations(t)
+			prescription, err := vehicle.ConductChecks("lightning", 23)
 			assert.Equal(t,
 				"lightning is all good",
 				prescription)
 			assert.Equal(t, nil, err)
 		})
 		t.Run("error", func(t *testing.T) {
-			testM.On("NeedsMaintenance", -45).Return(false, errors.New("Sample")).Once()
-			prescription, err := maintenanceRepository.ConductChecks("lightning", -45)
-			testM.AssertExpectations(t)
+			prescription, err := vehicle.ConductChecks("lightning", -45)
 			assert.Equal(t, "", prescription)
-			assert.Equal(t, errors.New("Sample"), err)
+			assert.Equal(t, errors.New("cannot accept less than zero days"), err)
 		})
 	})
 }
